6f30243b477f22acc4d342ce197e9991c985cb87
Sourayaak <sourayaabdelkader@gmail.com>
2022-10-11 20:03:55 +0300

    Frontend: Login: Fixing the layout of the login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 //simport logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
-import LoginForm from './components/Login'
+import LoginForm from './components/Login';
 
 function App() {
   return (
-    
     <div className="App">
       <LoginForm />
     </div>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,37 +1,37 @@
 export default function LoginForm () {
-    return <>
-        <div id="loginform">
-          <FormHeader title="Intelligence growth" />
-          <Form />
-        </div>
-        </>
-    }
-  
-  const FormHeader = props => (
-    <div>
-        <img id="icon" src="brain.png" alt="logo"></img>
-        <h2 id="headerTitle">{props.title}</h2>
-    </div>
-  );
-  
-  
-  const Form = props => (
-     <div>
-       <FormInput description="Email" placeholder="Enter your email" type="text" />
-       <FormInput description="Password" placeholder="Enter your password" type="password"/>
-       <FormButton title="Log in"/>
-     </div>
-  );
-  
-  const FormButton = props => (
-    <div id="button" class="row">
-      <button>{props.title}</button>
-    </div>
-  );
-  
-  const FormInput = props => (
-    <div class="row">
-      <label>{props.description}</label>
-      <input type={props.type} placeholder={props.placeholder}/>
-    </div>  
-  );
+  return <>
+      <div id="loginform">
+        <FormHeader title="Intelligence growth" />
+        <Form />
+      </div>
+      </>
+  }
+
+const FormHeader = props => (
+  <div>
+      <img id="icon" src="brain.png" alt="logo"></img>
+      <h2 id="headerTitle">{props.title}</h2>
+  </div>
+);
+
+
+const Form = props => (
+   <div>
+     <FormInput description="Email" placeholder="Enter your email" type="text" />
+     <FormInput description="Password" placeholder="Enter your password" type="password"/>
+     <FormButton title="Log in"/>
+   </div>
+);
+
+const FormButton = props => (
+  <div id="button" class="row">
+    <button>{props.title}</button>
+  </div>
+);
+
+const FormInput = props => (
+  <div class="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder}/>
+  </div>  
+);
