dc83249b3e752dce7a1f169fb9078f49ea235186
Sourayaak <sourayaabdelkader@gmail.com>
2022-10-11 11:20:00 +0300

    Backend: Courses: Adding the course table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,33 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+return new class extends Migration{
+    public function up(){
+
+        Schema::create('users', function ($collection) {
+            $collection->id();
+            $collection->string('full_name', 100);
+            $collection->string('email')->unique();
+            $collection->timestamp('password');
+            $collection->enum('user_type', ['admin', 'instructor', 'student']);
+            $collection->enum('graduated', ['yes', 'no'])->default('no');
+            $collection->string('picture_url')->nullable();
+            $collection->rememberToken();
+            $collection->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('users');
     }
 };
---
+++ backend/database/migrations/2022_10_10_141947_create_courses_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+
+    public function up(){
+        Schema::create('courses', function ($collection) {
+            $collection->id();
+            $collection->string("name", 100)->unique();
+            $collection->string("code", 30)->unique();
+            $collection->integer("credits", 2);
+            $collection->integer("hours", 3);
+            $collection->date("begin_at");
+            $collection->date("end_at");
+            $collection->timestamps();
+        });
+    }
+
+    public function down(){
+        Schema::dropIfExists('courses');
+    }
+};
