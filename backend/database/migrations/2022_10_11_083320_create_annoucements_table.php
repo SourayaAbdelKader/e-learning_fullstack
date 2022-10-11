77acb27547e05b2fd4a559c0e5a03288b08bb002
Sourayaak <sourayaabdelkader@gmail.com>
2022-10-11 11:35:19 +0300

    Backend: Annoucements: Adding the annoucements table

---
+++ backend/database/migrations/2022_10_11_083320_create_annoucements_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+    public function up(){
+        Schema::create('annoucements', function ($collection) {
+            $collection->id();
+            $collection->foreign('course_id')->references('id')->on('courses');
+            $collection->text('description');
+            $collection->timestamps();
+        });
+    }
+
+    public function down(){
+        Schema::dropIfExists('annoucements');
+    }
+};
